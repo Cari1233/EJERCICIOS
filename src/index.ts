d307225c77bcb11c5afafe0640b92de8f452f41a
Carina Martinez <92064265+Cari1233@users.noreply.github.com>
2022-01-17 16:25:08 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//EJECICIO 1 10% DE DESCUENTO
+
+//let precio: number = 450.5;
+//let descuento: number = precio * 0.1;
+//let precioFinal: number = precio - descuento;
+//console.log("por gastar mas de:", precio, "tiene un 10 % de descuento");
+//console.log("El precio final es:", precioFinal);
+
+//ejercicio 2
+let tiempo: number = 0;
+let suma: number = 0;
+let promedio: number = 0;
+for (let i: number = 0; i < 4; i++) {
+ // tiempo = Number(prompt("Tiempo de vuelta"));
+  suma = suma + tiempo;
+  console.log(tiempo);
+}
+promedio = suma / 4;
+console.log(promedio);
+console.log(suma);
